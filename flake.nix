18d7d7b0450fe0c9ccdcabd16a4feaba121c6d55
Damian Jordanov <d.jordanov@posteo.net>
2026-06-25 12:08:13 +0200

    include sqlc generated code so that building and running via nix works out

--- flake.nix
+++ flake.nix
@@ -1,84 +1,26 @@
 {
   description = "A simple CRUD web app in Golang";
 
-  # Nixpkgs / NixOS version to use.
   inputs.nixpkgs.url = "nixpkgs/nixos-26.05";
 
-  outputs =
-    { self, nixpkgs }:
+  outputs = { nixpkgs, ... }:
     let
-
-      # to work with older version of flakes
-      lastModifiedDate = self.lastModifiedDate or self.lastModified or "19700101";
-
-      # Generate a user-friendly version number.
-      version = builtins.substring 0 9 lastModifiedDate;
-
-      # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
-      forAllSystems = nixpkgs.lib.genAttrs [ "x86_64-linux" ];
-
-      # Nixpkgs instantiated for supported system types.
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in
     {
+      packages.${system} = {
+        default = pkgs.buildGoModule {
+          pname = "bpo";
+          version = "0.1.0";
+          src = ./.;
+          vendorHash = "sha256-zNiOubWRZOTJFyhcmM+lKdI954hfEgrpikM8pIyabTk=";
+        };
+      };
 
-      # Provide some binary packages for selected system types.
-      packages = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          bpo = pkgs.buildGoModule {
-            pname = "bpo";
-            inherit version;
-            # In 'nix develop', we don't need a copy of the source tree
-            # in the Nix store.
-            src = ./.;
-
-            # This hash locks the dependencies of this package. It is
-            # necessary because of how Go requires network access to resolve
-            # VCS.  See https://www.tweag.io/blog/2021-03-04-gomod2nix/ for
-            # details. Normally one can build with a fake hash and rely on native Go
-            # mechanisms to tell you what the hash should be or determine what
-            # it should be "out-of-band" with other tooling (eg. gomod2nix).
-            # To begin with it is recommended to set this, but one must
-            # remember to bump this hash when your dependencies change.
-            vendorHash = "sha256-xDIoz3NYvZSljEL2/MdrRE38z5NuxOYTDa5Xy2zbk50=";
-          };
-        }
-      );
-
-      # Add dependencies that are only needed for development
-      devShells = forAllSystems (
-        system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in
-        {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              go
-              sqlc
-              gopls
-              gotools
-              go-tools
-              delve
-              air
-            ];
-
-          shellHook = ''
-            echo "Entered Flake"
-          '';
-
-          };
-        }
-      );
-
-      # The default package for 'nix build'. This makes sense if the
-      # flake provides only one package or there is a clear "main"
-      # package.
-      defaultPackage = forAllSystems (system: self.packages.${system}.bpo);
+      devShells.${system}.default = pkgs.mkShell {
+        packages = with pkgs; [ go sqlc gopls gotools go-tools delve air ];
+        shellHook = "echo 'Entered Flake'";
+      };
     };
 }
