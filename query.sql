ac1596aaff546824b189465667bc9a8bbdd88a27
Damian Jordanov <d.jordanov@posteo.net>
2026-06-25 22:04:12 +0200

    start on persisting users and sessions

--- query.sql
+++ query.sql
@@ -1,3 +1,26 @@
+-- name: GetSessionById :one
+SELECT
+    id,
+    user,
+    expires_at
+FROM session
+WHERE id = $1
+;
+
+-- name: CreateUser :one
+INSERT INTO user
+    (email, created_at)
+VALUES ($1, $2)
+RETURNING *
+;
+
+-- name: CreateSession :one
+INSERT INTO session
+    (id, user, expires_at)
+VALUES ($1, $2, $3)
+RETURNING *
+;
+
 -- name: GetBloodPressureObservations :many
 SELECT
     id,
@@ -8,13 +31,14 @@ SELECT
     irregular,
     comment
 FROM blood_pressure_observation
+WHERE user = $1
 ;
 
 -- name: CreateBloodPressureObservation :one
 INSERT INTO blood_pressure_observation
-    (observed_at, systolic, diastolic, pulse, irregular, comment)
+    (observed_at, systolic, diastolic, pulse, irregular, comment, user)
 VALUES
-    ($1, $2, $3, $4, $5, $6)
+    ($1, $2, $3, $4, $5, $6, $7)
 RETURNING *
 ;
 
@@ -28,10 +52,12 @@ SET
     irregular = $5,
     comment = $6
 WHERE id = $7
+    AND user = $8
 RETURNING *
 ;
 
 -- name: DeleteBloodPressureObservation :exec
 DELETE FROM blood_pressure_observation
 WHERE id = $1
+    AND user = $2
 ;
--- schema.sql
+++ schema.sql
@@ -1,9 +1,22 @@
 CREATE TABLE blood_pressure_observation (
     id SERIAL PRIMARY KEY,
+    user REFERENCES user(id) NOT NULL,
     observed_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     systolic INTEGER NOT NULL,
     diastolic INTEGER NOT NULL,
     pulse INTEGER NOT NULL,
     irregular BOOLEAN NOT NULL DEFAULT false,
     comment TEXT NOT NULL DEFAULT ''
+);
+
+CREATE TABLE user (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE session (
+    id UUID NOT NULL,
+    user REFERENCES user(id) NOT NULL,
+    expires_at TIMESTAMP NOT NULL
 );
