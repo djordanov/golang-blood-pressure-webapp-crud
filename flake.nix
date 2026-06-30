512dd73c6dac52c88f476dd20128fb3878b0b67a
Damian Jordanov <d.jordanov@posteo.net>
2026-06-30 11:53:28 +0200

    add container to flake.nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,32 @@
 {
-  description = "A simple CRUD web app in Golang";
+  description = "A simple blood pressure CRUD web app in Golang";
 
   inputs.nixpkgs.url = "nixpkgs/nixos-26.05";
 
-  outputs = { nixpkgs, ... }:
+  outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages.${system};
     in
     {
       packages.${system} = {
-        default = pkgs.buildGoModule {
+        bpo = pkgs.buildGoModule {
           pname = "bpo";
-          version = "0.1.0";
+          version = "0.2.0";
           src = ./.;
           vendorHash = "sha256-zNiOubWRZOTJFyhcmM+lKdI954hfEgrpikM8pIyabTk=";
         };
+        container = pkgs.dockerTools.buildImage {
+          name = "bpo-container";
+          tag = "latest";
+          contents = [ self.packages.${system}.bpo];
+          config = {
+            Cmd = [ "${self.packages.${system}.bpo}/bin/bpo"];
+            ExposedPorts = { "8080/tcp" = {}; };
+          };
+        };
+
+        default = self.packages.${system}.bpo;
       };
 
       devShells.${system}.default = pkgs.mkShell {
