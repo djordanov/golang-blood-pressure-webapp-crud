d92b38f66e687e6940c2884d13ac5c553354cde3
Damian Jordanov <d.jordanov@posteo.net>
2026-07-31 13:18:27 +0200

    refactoring and layout improvements

--- static/app.js
+++ static/app.js
@@ -1,14 +1,14 @@
 function setDefaultDateTime() {
   let now = new Date();
-  document.querySelector('#bpo-new-date').value
+  document.querySelector('#bpo-date').value
     = `${now.getFullYear()}-${(now.getMonth() + 1).toString().padStart(2, "0")}-${now.getDate().toString().padStart(2, "0")}`;
-  document.querySelector('#bpo-new-time').value
+  document.querySelector('#bpo-time').value
     = `${now.getHours().toString().padStart(2, "0")}:${now.getMinutes().toString().padStart(2, "0")}`;
 }
 
 // Close modal when clicking outside (on the backdrop)
 document.addEventListener('click', (event) => {
-  const modal = document.querySelector('#add-new-bpo-dialog');
+  const modal = document.querySelector('#bpo-CRUD-dialog');
   if (modal && modal.open && event.target === modal) {
     modal.close();
   }
